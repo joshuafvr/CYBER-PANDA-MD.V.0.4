899381218b0316de9e2304037655b41ba7a655fa
joshuafvr <74902842+joshuafvr@users.noreply.github.com>
2024-03-23 11:40:09 +0100

    Update config.js

--- config.js
+++ config.js
@@ -21,11 +21,11 @@ global.prefa = ["."];                                    //Default prefix here. 
 
 
 //
-global.Owner = ["+94704274987", "+94704274987"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.OwnerNumber = ["+94704274987", "+94704274987"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.ownertag = ["+94704274987"];
-global.OwnerName = "SACHITH";
-global.BotName = "SACHITH";
+global.Owner = ["+2349021325614"];         //If you want singal number so global.Owner = ['9021325614'] Change into your number.
+global.OwnerNumber = ["+2349021325614"];   //If you want singal number so global.Owner = ['9021325614'] Change into your number.
+global.ownertag = ["+2349021325614"];
+global.OwnerName = "unbothered";
+global.BotName = "unbothered";
 global.packname = "🦋 │𝐂𝐘𝐁𝜩𝐑│𝐏𝜟𝐍𝐃𝐀│𝐌𝐃│𝐕➂ 🦋";                             //Do not change.
 global.author = "SACHITH";                               //Do not change.
 global.BotSourceCode = "https://github.com/Kai00"; //Do not change.
